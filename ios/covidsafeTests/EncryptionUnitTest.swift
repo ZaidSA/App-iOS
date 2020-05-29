bc1cdf4374e66f695ec33aa0ca3f06e0a14d574f
Paresh Prajapati <paresh.prajapati.sa@gmail.com>
2020-05-29 19:06:58 +0530

    Rename app

--- ios/covidsafeTests/EncryptionUnitTest.swift
+++ ios/covidsafeTests/EncryptionUnitTest.swift
@@ -1,31 +1,51 @@
 import XCTest
-@testable import covidsafe
+//@testable import covidsafe
 
 class EncryptionUnitTest: XCTestCase {
 
-    override func setUpWithError() throws {
+    override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
-      super.setUp()
-      
     }
 
-    override func tearDownWithError() throws {
+    override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-//      let util = EncryptionUtil();
-//      let result = try util.encrypt(plainText: "-40.12345")
-//      print("result: \(result)")
-//      let origin = try util.decrypt(encryptedString: result)
-//      print("origin: \(origin)")
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 
-    func testPerformanceExample() throws {
+    func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measure {
             // Put the code you want to measure the time of here.
         }
     }
+
+//     override func setUpWithError() throws {
+//         // Put setup code here. This method is called before the invocation of each test method in the class.
+//       super.setUp()
+      
+//     }
+
+//     override func tearDownWithError() throws {
+//         // Put teardown code here. This method is called after the invocation of each test method in the class.
+//     }
+
+//     func testExample() throws {
+// //      let util = EncryptionUtil();
+// //      let result = try util.encrypt(plainText: "-40.12345")
+// //      print("result: \(result)")
+// //      let origin = try util.decrypt(encryptedString: result)
+// //      print("origin: \(origin)")
+//     }
+
+//     func testPerformanceExample() throws {
+//         // This is an example of a performance test case.
+//         self.measure {
+//             // Put the code you want to measure the time of here.
+//         }
+//     }
 
 }
