31b8e1ad4d7f08e07518444e69c83c3b3affaa7c
Paresh Prajapati <paresh.prajapati.sa@gmail.com>
2020-05-30 13:01:16 +0530

    Added icon

--- utils/endpoints.js
+++ utils/endpoints.js
@@ -1,3 +1,4 @@
-export const GET_MESSAGE_LIST_URL = 'https://csapi.azurefd.net/api/Messages/List';
+export const GET_MESSAGE_LIST_URL = 'https://sacovidsafe.azurewebsites.net/api/Messages/List';
 
-export const FETCH_MESSAGE_INFO_URL = 'https://csapi.azurefd.net/api/Message';
+export const FETCH_MESSAGE_INFO_URL = 'https://sacovidsafe.azurewebsites.net/api/Message';
+// "https://sacovidsafe.azurewebsites.net/swagger/index.html"
